0361b5621bd4ae752ec310bc68a972383c3fd0e3
Davit Melikyan <davidmelikyan900@gmail.com>
2025-06-07 18:19:41 +0400

    Bank Account Managment Task

--- BankAccount.h
+++ BankAccount.h
@@ -9,27 +9,11 @@ private:
 	int accountNumber;  
 	double balance;  
 public:
-	BankAccount() {
-		accountHolder = "Unknown";
-		accountNumber = 0;
-		balance = 0.0;
-	} 
-	BankAccount(std::string Name, int Number, double initialBalance) {
-		accountHolder = Name;
-		accountNumber = Number;
-		balance = initialBalance;
-	}
-	BankAccount(const BankAccount& src) {
-		accountHolder = src.accountHolder;
-		accountNumber = src.accountNumber;
-		balance = src.balance;
-	}
-	BankAccount(std::string& Name, int Number) : accountHolder(Name), accountNumber(Number), balance(0.0) {
-		
-	}
-	~BankAccount() {
-	
-	}	
+	BankAccount() : accountHolder{"Unknown"}, accountNumber{0}, balance{0.0} {}
+	BankAccount(std::string Name, int Number, double initialBalance) : accountHolder{Name}, accountNumber{Number}, balance{initialBalance} {}
+	BankAccount(const BankAccount& src) : accountHolder{src.accountHolder}, accountNumber{src.accountNumber}, balance{src.balance} {}
+	BankAccount(std::string& Name, int Number) : accountHolder(Name), accountNumber(Number), balance(0.0) {}
+	~BankAccount() = default;
 	void deposit(double amount);
 	bool withdraw(double amount);
 	void display() const;
